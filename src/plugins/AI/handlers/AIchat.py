cb95a64e9402407da1f0aed9a07b0ff2f405a098
lym2006 <3280603325@qq.com>
2026-03-29 21:48:41 +0800

    修复AI对话一轮后失效

--- src/plugins/AI/handlers/AIchat.py
+++ src/plugins/AI/handlers/AIchat.py
@@ -6,7 +6,7 @@ from aiogram.filters import Filter
 from aiogram.enums import ChatType,ContentType
 
 from ..glo import (
-        session_guard,user_session,active_tasks,
+        session_guard,user_session,active_tasks,TaskQueue,
         GROUP_TRIGGERS,get_name,TaskItem
     )
 from ..services.monitor import monitor_loop
@@ -38,6 +38,8 @@ async def AIchat(message:Message,bot:Bot):
     if user in black_list:
         return
     session=user_session[user]
+    if not 'queue' in session:
+        session['queue']=TaskQueue()
     queue=session['queue']
     session['md']=True
     task=TaskItem(message)
